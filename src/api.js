c957919927ddca52209659e2f612a5b14a9b9cd0
Murilo M Andrade <andrade.murilom@gmail.com>
2023-08-27 13:54:43 -0300

    Criado a conexao com a api stripe e instaciada com o axios

---
+++ src/api.js
@@ -0,0 +1,14 @@
+const axios = require('axios')
+require('dotenv').config();
+
+
+const instanciaAxios = axios.create({
+    baseURL: 'https://api.stripe.com/v1',
+    headers: {
+        authorization: `Bearer ${process.env.API_KEY}`,
+        'Content-type': 'application/x-www-form-urlencoded'
+
+    }
+})
+
+module.exports = instanciaAxios
