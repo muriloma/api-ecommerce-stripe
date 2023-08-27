7f7cfe7024c193fa54305400146d5991a338aabd
Murilo M Andrade <andrade.murilom@gmail.com>
2023-08-27 13:26:43 -0300

    Criado controladores de clientes com endpoints de listar e cadastrar

--- src/rotas.js
+++ src/rotas.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const clientes = require('./controladores/clientes')
 
 const rotas = express()
+
+rotas.get('/cliente', clientes.listar)
 
 
 module.exports = rotas
